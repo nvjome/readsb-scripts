019ce4e6ff3a5acc715038f74adfa931dc4d3ce7
Nolan Jome <94138462+nvjome@users.noreply.github.com>
2023-07-27 20:58:51 -0500

    Update stop-readsb.sh

---
+++ stop-readsb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo systemctl stop readsb.service tar1090.service lighttpd.service autogain1090.timer
--- stop_readsb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo systemctl stop readsb tar1090 lighttpd autogain1090.timer
